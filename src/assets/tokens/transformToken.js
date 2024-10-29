6fbb0928163f3109288a7c2e071cbb26297dfa18
유재우 <yoo8960@hconnect.co.kr>
2024-10-29 14:50:37 +0900

    init6

--- src/assets/tokens/transformToken.js
+++ src/assets/tokens/transformToken.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+// 빌드 경로가 없으면 생성
+const buildPath = path.join(__dirname, 'build/scss');
+if (!fs.existsSync(buildPath)) {
+    fs.mkdirSync(buildPath, { recursive: true });
+}
+
 const StyleDictionary = require('style-dictionary').extend({
     source: ['./src/assets/tokens/tokens.json'],
     platforms: {
